97ae3d3c6e188b83c92d256503b42b3cbc02c393
Ruan-Xin <415131215@qq.com>
2018-05-27 16:22:01 +0800

    init

---
+++ dubbomesh-consumer/src/main/java/com/roy/dubbomesh/remoting/client/Client.java
@@ -0,0 +1,9 @@
+package com.roy.dubbomesh.remoting.client;
+
+/**
+ * @author ruanxin
+ * @create 2018-05-26
+ * @desc
+ */
+public interface Client {
+}
---
+++ dubbomesh-consumer/src/main/java/com/roy/dubbomesh/remoting/server/Server.java
@@ -0,0 +1,9 @@
+package com.roy.dubbomesh.remoting.server;
+
+/**
+ * @author ruanxin
+ * @create 2018-05-26
+ * @desc
+ */
+public interface Server {
+}
